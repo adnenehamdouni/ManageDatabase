db8769e82b7984f041811b53321ab7a6350e39e9
Adnene HAMDOUNI <adnene.hamdouni@qugenx.com>
2019-05-26 10:55:39 +0200

    first commit

---
+++ src/main/java/com/qugenx/managedatabase/ManageDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.qugenx.managedatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageDatabaseApplication.class, args);
+    }
+
+}
